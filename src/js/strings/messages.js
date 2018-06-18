9ebc6e63932f9e3d267c3a4523bedde90b04afbd
Jon Chretien <jon.chretien@gmail.com>
2018-06-18 18:18:19 -0400

    feature detection & strings

---
+++ src/js/strings/messages.js
@@ -0,0 +1,10 @@
+/*
+ * String Messages
+ *
+ */
+
+export default (messages = {
+  touch: 'It looks like you&rsquo;re on a touch device. This site currently works on desktop browsers only.',
+  instructions: 'Drag a photo from your desktop into the box below',
+  support: 'It looks like your browser doesn&rsquo;t support the features this site needs to work. Download the latest versions of <a href="https://www.google.com/chrome" target="_blank">Google Chrome</a> or <a href="http://www.mozilla.org/firefox/" target="_blank">Mozilla Firefox</a> in order to view it.',
+});
