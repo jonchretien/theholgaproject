69a492eb1b33cbf325a1f8eed0d9bea6a0aa33d8
Jon Chretien <jon.chretien@gmail.com>
2018-06-18 23:11:04 -0400

    check browser support logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
-import { greeting } from './modules/greeting';
+import {
+  hasCanvasSupport,
+  hasDragAndDropSupport,
+  hasFileReaderSupport,
+  isTouchDevice,
+} from './lib/support';
 
-greeting('📷');
+/**
+ * Runs support tests to determine if app will work with the user's browser.
+ * Displays alerts if browser doesn't meet requirements.
+ */
+function checkBrowserSupport() {
+  if (hasCanvasSupport() && hasDragAndDropSupport() && hasFileReaderSupport()) {
+    console.log('cuts the mustard');
+    return;
+  }
+
+  if (isTouchDevice()) {
+    console.log('is touch device');
+    return;
+  }
+
+  console.log('no support');
+}
+
+checkBrowserSupport();
