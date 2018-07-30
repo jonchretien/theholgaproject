97f03791b777560c3c30792eebd4bffcc306c7b8
Jon Chretien <jon.chretien@gmail.com>
2018-07-30 13:47:55 -0400

    build & deploy scripts

--- bin/timestamp-bundles.js
+++ bin/timestamp-bundles.js
@@ -7,7 +7,9 @@ const fs = require('fs');
 const DIST_PATH = 'dist';
 const timestamp = Date.now();
 const minifiedCSS = `bundle.min.${timestamp}.css`;
+const minifiedJS = `bundle.min.${timestamp}.js`;
 const original = `${DIST_PATH}/index.html`;
+
 let page = null;
 let revised = null;
 
@@ -16,10 +18,13 @@ fs.renameSync(
   `${DIST_PATH}/css/bundle.min.css`,
   `${DIST_PATH}/css/${minifiedCSS}`
 );
+fs.renameSync(`${DIST_PATH}/js/bundle.min.js`, `${DIST_PATH}/js/${minifiedJS}`);
 
 // read file contents and update file paths
 page = fs.readFileSync(original, 'utf8');
-revised = page.replace('\/css\/bundle.css', `/css/${minifiedCSS}`);
+revised = page
+  .replace('\/css\/bundle.css', `/css/${minifiedCSS}`)
+  .replace('\/js\/bundle.js', `/js/${minifiedJS}`);
 
 // write new contents to file
 fs.writeFileSync(original, revised);
